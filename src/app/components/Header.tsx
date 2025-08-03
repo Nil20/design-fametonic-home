eb3ac2fad326ecd72da0afd717e073bc2304d302
Md. Ashikul Alam <ashikul.alam@dsinnovators.com>
2025-08-03 21:15:24 +0600

    chore: implement header mobile view

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,12 +2,22 @@
 
 export default function Header() {
   return (
-    <header className="w-full h-[46px] bg-[linear-gradient(to_right,#FC004E,#10CBE0)] text-white px-2 flex items-center justify-center overflow-x-auto">
-      <div className="font-figtree font-extrabold uppercase text-[22px] leading-[100%] text-sky-400 whitespace-nowrap text-center">
-        🚀 Fresh Beginnings Sale
-      </div>
-      <div className="font-figtree font-extrabold text-[22px] leading-[100%] whitespace-nowrap text-white ml-1">
-        : Extra 25% OFF, Limited Spots – start your journey today!
+    <header className="w-full h-[76px] md:h-[46px] bg-[linear-gradient(to_right,#FC004E,#10CBE0)] text-white px-2 flex items-center justify-center overflow-x-auto">
+      <div className="text-center">
+        <span className="font-figtree font-extrabold uppercase text-[22px] leading-[100%] text-sky-400 whitespace-nowrap hidden md:inline">
+          🚀 Fresh Beginnings Sale:
+        </span>
+        <span className="font-figtree font-extrabold text-[22px] leading-[100%] whitespace-nowrap text-white ml-1 hidden md:inline">
+          Extra 25% OFF, Limited Spots – start your journey today!
+        </span>
+
+        {/* Mobile version */}
+        <div className="font-figtree font-extrabold text-[16px] leading-[100%] text-white md:hidden text-center px-2">
+          <span className="text-sky-400 uppercase">
+            🚀 Fresh Beginnings Sale:
+          </span>{' '}
+          Extra 25% OFF, Limited Spots – start your journey today!
+        </div>
       </div>
     </header>
   );
