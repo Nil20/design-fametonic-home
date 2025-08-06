e36097c1b9f6b97494c3696bf1fd6bdacbc32e0f
Md. Ashikul Alam <ashikul.alam@dsinnovators.com>
2025-08-07 02:13:56 +0600

    center button and text in mobile view

--- src/app/components/body/Button.tsx
+++ src/app/components/body/Button.tsx
@@ -2,7 +2,7 @@
 
 export default function Button() {
   return (
-    <div className="flex flex-col gap-[10px]">
+    <div className="flex flex-col gap-[10px] items-center md:items-start">
       <button
         onClick={() => alert('Get Started clicked!')}
         className="w-[313px] h-[40px] bg-[#FC004E] text-white font-figtree text-[20px] button-shadow-fametonic font-bold leading-[100%] tracking-normal rounded-[10px] px-[40px] py-[8px] flex items-center justify-center"
