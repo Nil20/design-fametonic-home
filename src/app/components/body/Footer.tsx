5744e0885a169967700fb78147829cb180390c9d
Md. Ashikul Alam <ashikul.alam@dsinnovators.com>
2025-08-07 02:16:17 +0600

    align footer in the middle for mobile

--- src/app/components/body/Footer.tsx
+++ src/app/components/body/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <div className="mt-4 flex flex-col items-start w-[516px] gap-[12px]">
+    <div className="mt-4 flex flex-col items-center md:items-start w-[350px] md:w-[516px] gap-[12px]">
       <p
         className="font-figtree font-medium tracking-normal text-[#ABABAB] text-[12px] leading-[16px] break-normal"
         style={{
