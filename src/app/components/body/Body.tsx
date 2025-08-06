2ecfe043895a75bc8f9ebd915d3d852721abe9c6
Md. Ashikul Alam <ashikul.alam@dsinnovators.com>
2025-08-07 03:10:23 +0600

    amend mobile view for image

--- src/app/components/body/Body.tsx
+++ src/app/components/body/Body.tsx
@@ -2,7 +2,7 @@
 
 import Button from './Button';
 import Footer from './Footer';
-import ImageView from './Image';
+import ImageView from './ImageView';
 import List from './List';
 import TextDescription from './TextDescription';
 
@@ -10,7 +10,7 @@ export default function Body() {
   return (
     <section className="relative overflow-hidden md:min-h-[805px] md:pt-[212px] flex flex-col md:flex-row md:ml-[211px] items-center justify-between px-6 md:px-16 py-12 md:gap-10 gap-6">
       <ImageView />
-      <div className="flex flex-col md:top-[212px] top-0 md:h-[511px] w-full max-w-[516px] gap-[30px] z-10 mx-auto md:mx-0">
+      <div className="flex flex-col -mt-[90px] md:mt-0 w-full max-w-[516px] gap-[30px] z-10 mx-auto md:mx-0">
         <TextDescription />
         <List />
 
--- src/app/components/body/ImageView.tsx
+++ src/app/components/body/ImageView.tsx
@@ -13,12 +13,13 @@ export default function ImageView() {
         className="hidden md:block absolute top-[126px] right-[280px] z-0"
       />
 
-      <div className="block md:hidden w-full flex justify-center mb-6 z-0">
+      {/* Mobile Image - flows normally */}
+      <div className="block md:hidden flex justify-center w-full mb-6 z-0">
         <Image
           src="/assets/image.svg"
           alt="Hero Graphic"
-          width={0}
-          height={0}
+          width={390}
+          height={426}
           className="w-[390px] h-[426px] z-0"
         />
       </div>
